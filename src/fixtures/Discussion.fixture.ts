c607f3e3e9292e1a6072928f4a43d37abd16f6c5
Asone <nelson@herbin.info>
2018-12-10 08:43:06 +0100

    add: fixtures for tests

---
+++ src/fixtures/Discussion.fixture.ts
@@ -0,0 +1,805 @@
+/**
+ * @author Nelson Herbin <nelson@herbin.info>
+ * @copyright 2018. MIT Licence
+ * 
+ */
+
+import { DiscussionInterface, AmdtsParOrdreDeDiscussionInterface } from '../interfaces/Discussion.interface';
+
+export const discussionFixture: DiscussionInterface = {
+	amdtsParOrdreDeDiscussion: {
+		organe: "AN",
+		bibard: "1396",
+		bibardSuffixe: "",
+		legislature: "15",
+		titre: "Loi de programmation 2019-2022 et de réforme pour la justice",
+		type: "projet de loi",
+		divisions: [{
+			place: "Article PREMIER",
+			position: "1/1441",
+			amendements: null
+		}, {
+			place: "RAPPORT ANNEXÉ",
+			position: "19/1441",
+			amendements: null
+		}, {
+			place: "Article 1er bis",
+			position: "53/1441",
+			amendements: null
+		}, {
+			place: "Article 1er ter",
+			position: "55/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 1er ter",
+			position: "59/1441",
+			amendements: null
+		}, {
+			place: "SOUSTITRE Ier",
+			position: "60/1441",
+			amendements: null
+		}, {
+			place: "Avant l'article 2",
+			position: "61/1441",
+			amendements: null
+		}, {
+			place: "Article 2",
+			position: "62/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 2",
+			position: "98/1441",
+			amendements: null
+		}, {
+			place: "Article 3",
+			position: "106/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 3",
+			position: "135/1441",
+			amendements: null
+		}, {
+			place: "Article 4",
+			position: "139/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 4",
+			position: "191/1441",
+			amendements: null
+		}, {
+			place: "Article 5",
+			position: "224/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 5",
+			position: "262/1441",
+			amendements: null
+		}, {
+			place: "Article 6",
+			position: "263/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 6",
+			position: "312/1441",
+			amendements: null
+		}, {
+			place: "Article 7",
+			position: "315/1441",
+			amendements: null
+		}, {
+			place: "Article 8",
+			position: "331/1441",
+			amendements: null
+		}, {
+			place: "Article 8 bis",
+			position: "339/1441",
+			amendements: null
+		}, {
+			place: "Article 8 ter",
+			position: "342/1441",
+			amendements: null
+		}, {
+			place: "Article 8 quater",
+			position: "345/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 8 quater",
+			position: "349/1441",
+			amendements: null
+		}, {
+			place: "Article 9",
+			position: "352/1441",
+			amendements: null
+		}, {
+			place: "Article 9 ter",
+			position: "353/1441",
+			amendements: null
+		}, {
+			place: "Article 10",
+			position: "355/1441",
+			amendements: null
+		}, {
+			place: "Article 10 bis",
+			position: "358/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 10 bis",
+			position: "359/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 11",
+			position: "360/1441",
+			amendements: null
+		}, {
+			place: "Article 12",
+			position: "363/1441",
+			amendements: null
+		}, {
+			place: "Article 12 bis A",
+			position: "397/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 12 bis A",
+			position: "400/1441",
+			amendements: null
+		}, {
+			place: "Article 12 bis",
+			position: "408/1441",
+			amendements: null
+		}, {
+			place: "Article 12 ter",
+			position: "413/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 12 ter",
+			position: "419/1441",
+			amendements: null
+		}, {
+			place: "Article 13",
+			position: "431/1441",
+			amendements: null
+		}, {
+			place: "Article 14",
+			position: "437/1441",
+			amendements: null
+		}, {
+			place: "Article 15",
+			position: "452/1441",
+			amendements: null
+		}, {
+			place: "Article 16",
+			position: "461/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 16",
+			position: "462/1441",
+			amendements: null
+		}, {
+			place: "Article 17",
+			position: "463/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 17",
+			position: "486/1441",
+			amendements: null
+		}, {
+			place: "Article 18",
+			position: "487/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 18",
+			position: "495/1441",
+			amendements: null
+		}, {
+			place: "Article 18 bis",
+			position: "499/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 18 bis",
+			position: "501/1441",
+			amendements: null
+		}, {
+			place: "Article 19",
+			position: "505/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 19",
+			position: "511/1441",
+			amendements: null
+		}, {
+			place: "Avant l'article 19 bis",
+			position: "514/1441",
+			amendements: null
+		}, {
+			place: "Article 19 bis",
+			position: "515/1441",
+			amendements: null
+		}, {
+			place: "Article 19 ter",
+			position: "519/1441",
+			amendements: null
+		}, {
+			place: "Article 19 quater",
+			position: "522/1441",
+			amendements: null
+		}, {
+			place: "Article 20 A",
+			position: "524/1441",
+			amendements: null
+		}, {
+			place: "Article 20",
+			position: "525/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 20",
+			position: "526/1441",
+			amendements: null
+		}, {
+			place: "Article 21",
+			position: "532/1441",
+			amendements: null
+		}, {
+			place: "Article 22",
+			position: "540/1441",
+			amendements: null
+		}, {
+			place: "Article 22 bis",
+			position: "547/1441",
+			amendements: null
+		}, {
+			place: "Avant l'article 24",
+			position: "548/1441",
+			amendements: null
+		}, {
+			place: "Article 24",
+			position: "550/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 24",
+			position: "553/1441",
+			amendements: null
+		}, {
+			place: "Article 25",
+			position: "555/1441",
+			amendements: null
+		}, {
+			place: "Article 25 bis A",
+			position: "557/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 25 bis A",
+			position: "560/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 25 quater",
+			position: "565/1441",
+			amendements: null
+		}, {
+			place: "Article 26",
+			position: "566/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 26",
+			position: "589/1441",
+			amendements: null
+		}, {
+			place: "Article 26 bis",
+			position: "599/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 26 bis",
+			position: "600/1441",
+			amendements: null
+		}, {
+			place: "Article 27",
+			position: "603/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 27",
+			position: "611/1441",
+			amendements: null
+		}, {
+			place: "Article 28",
+			position: "612/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 28",
+			position: "618/1441",
+			amendements: null
+		}, {
+			place: "Article 29",
+			position: "619/1441",
+			amendements: null
+		}, {
+			place: "Article 30",
+			position: "637/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 30",
+			position: "642/1441",
+			amendements: null
+		}, {
+			place: "Article 31",
+			position: "646/1441",
+			amendements: null
+		}, {
+			place: "Article 31 bis",
+			position: "657/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 31 bis",
+			position: "661/1441",
+			amendements: null
+		}, {
+			place: "Article 32",
+			position: "662/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 32",
+			position: "682/1441",
+			amendements: null
+		}, {
+			place: "Article 32 bis",
+			position: "684/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 32 bis",
+			position: "685/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 32 ter",
+			position: "689/1441",
+			amendements: null
+		}, {
+			place: "Avant l'article 52",
+			position: "690/1441",
+			amendements: null
+		}, {
+			place: "Article 52",
+			position: "723/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 52",
+			position: "736/1441",
+			amendements: null
+		}, {
+			place: "Article 33",
+			position: "740/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 33",
+			position: "744/1441",
+			amendements: null
+		}, {
+			place: "Article 34",
+			position: "750/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 34",
+			position: "763/1441",
+			amendements: null
+		}, {
+			place: "Article 35",
+			position: "765/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 35",
+			position: "780/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 35 bis",
+			position: "785/1441",
+			amendements: null
+		}, {
+			place: "Article 36",
+			position: "787/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 36",
+			position: "797/1441",
+			amendements: null
+		}, {
+			place: "Article 37",
+			position: "798/1441",
+			amendements: null
+		}, {
+			place: "Article 38",
+			position: "838/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 38",
+			position: "863/1441",
+			amendements: null
+		}, {
+			place: "Avant l'article 39",
+			position: "864/1441",
+			amendements: null
+		}, {
+			place: "Article 39",
+			position: "865/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 39",
+			position: "882/1441",
+			amendements: null
+		}, {
+			place: "Article 40",
+			position: "883/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 40",
+			position: "892/1441",
+			amendements: null
+		}, {
+			place: "Article 41",
+			position: "893/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 41",
+			position: "903/1441",
+			amendements: null
+		}, {
+			place: "Article 42",
+			position: "904/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 42",
+			position: "931/1441",
+			amendements: null
+		}, {
+			place: "Article 42 bis AA",
+			position: "935/1441",
+			amendements: null
+		}, {
+			place: "Article 42 bis AB",
+			position: "962/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 42 bis AC",
+			position: "964/1441",
+			amendements: null
+		}, {
+			place: "Article 42 bis A",
+			position: "968/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 42 bis A",
+			position: "970/1441",
+			amendements: null
+		}, {
+			place: "Article 42 bis B",
+			position: "972/1441",
+			amendements: null
+		}, {
+			place: "Article 42 bis C",
+			position: "974/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 42 bis C",
+			position: "980/1441",
+			amendements: null
+		}, {
+			place: "Article 42 bis",
+			position: "983/1441",
+			amendements: null
+		}, {
+			place: "Avant l'article 43",
+			position: "985/1441",
+			amendements: null
+		}, {
+			place: "Article 43",
+			position: "987/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 43",
+			position: "1024/1441",
+			amendements: null
+		}, {
+			place: "Article Article 43 bis",
+			position: "1030/1441",
+			amendements: null
+		}, {
+			place: "Article 43 ter",
+			position: "1034/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 43 ter",
+			position: "1035/1441",
+			amendements: null
+		}, {
+			place: "Article 43 quater",
+			position: "1052/1441",
+			amendements: null
+		}, {
+			place: "Article 44",
+			position: "1056/1441",
+			amendements: null
+		}, {
+			place: "Article 45",
+			position: "1060/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 45",
+			position: "1086/1441",
+			amendements: null
+		}, {
+			place: "Article 45 bis A",
+			position: "1091/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 45 bis A",
+			position: "1096/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 45 bis B",
+			position: "1099/1441",
+			amendements: null
+		}, {
+			place: "Article 45 ter",
+			position: "1100/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 45 ter",
+			position: "1101/1441",
+			amendements: null
+		}, {
+			place: "Article 45 quater",
+			position: "1104/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 45 quater",
+			position: "1105/1441",
+			amendements: null
+		}, {
+			place: "Avant l'article 46",
+			position: "1108/1441",
+			amendements: null
+		}, {
+			place: "Article 46",
+			position: "1110/1441",
+			amendements: null
+		}, {
+			place: "Article 47",
+			position: "1121/1441",
+			amendements: null
+		}, {
+			place: "Article 48",
+			position: "1127/1441",
+			amendements: null
+		}, {
+			place: "Article 48 bis",
+			position: "1135/1441",
+			amendements: null
+		}, {
+			place: "Article 49",
+			position: "1138/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 49",
+			position: "1143/1441",
+			amendements: null
+		}, {
+			place: "Article 49 bis A",
+			position: "1146/1441",
+			amendements: null
+		}, {
+			place: "Article 49 bis",
+			position: "1147/1441",
+			amendements: null
+		}, {
+			place: "Article 50",
+			position: "1148/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 50",
+			position: "1167/1441",
+			amendements: null
+		}, {
+			place: "Article 50 bis A",
+			position: "1173/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 50 bis",
+			position: "1174/1441",
+			amendements: null
+		}, {
+			place: "Article 50 ter",
+			position: "1177/1441",
+			amendements: null
+		}, {
+			place: "Article 50 quater",
+			position: "1179/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 50 quater",
+			position: "1181/1441",
+			amendements: null
+		}, {
+			place: "Article 53",
+			position: "1188/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 53",
+			position: "1312/1441",
+			amendements: null
+		}, {
+			place: "Article 53 bis B",
+			position: "1320/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 53 bis B",
+			position: "1325/1441",
+			amendements: null
+		}, {
+			place: "Article 53 bis",
+			position: "1327/1441",
+			amendements: null
+		}, {
+			place: "Article 54",
+			position: "1328/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 54",
+			position: "1342/1441",
+			amendements: null
+		}, {
+			place: "Article 55",
+			position: "1343/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 55 bis",
+			position: "1352/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 50 quater",
+			position: "1354/1441",
+			amendements: null
+		}, {
+			place: "Avant l'article 51",
+			position: "1359/1441",
+			amendements: null
+		}, {
+			place: "Article 51",
+			position: "1360/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 51",
+			position: "1371/1441",
+			amendements: null
+		}, {
+			place: "Article 51 bis",
+			position: "1377/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 51 bis",
+			position: "1380/1441",
+			amendements: null
+		}, {
+			place: "Article 51 ter",
+			position: "1381/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 51 ter",
+			position: "1384/1441",
+			amendements: null
+		}, {
+			place: "Article 52 bis",
+			position: "1398/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 52 bis",
+			position: "1401/1441",
+			amendements: null
+		}, {
+			place: "Article 52 ter",
+			position: "1404/1441",
+			amendements: null
+		}, {
+			place: "Article 52 quater",
+			position: "1406/1441",
+			amendements: null
+		}, {
+			place: "Article 52 quinquies",
+			position: "1408/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 52 quinquies",
+			position: "1410/1441",
+			amendements: null
+		}, {
+			place: "Article 56",
+			position: "1411/1441",
+			amendements: null
+		}, {
+			place: "Article 57",
+			position: "1415/1441",
+			amendements: null
+		}, {
+			place: "Après l'article 57",
+			position: "1418/1441",
+			amendements: null
+		}],
+		amendements: [{
+			place: "Article 54",
+			sort: "Retiré",
+			numero: "1472",
+			parentNumero: "",
+			auteurLabel: "Mme AVIA",
+			auteurGroupe: "LaREM",
+			alineaLabel: "1",
+			missionLabel: "",
+			discussionCommune: "5187",
+			discussionCommuneAmdtPositon: "fin",
+			discussionCommuneSsAmdtPositon: "",
+			discussionIdentique: "",
+			discussionIdentiqueAmdtPositon: "",
+			discussionIdentiqueSsAmdtPositon: "",
+			position: "1340/1441"
+		}, {
+			place: "Article 54",
+			sort: "Rejeté",
+			numero: "1562",
+			parentNumero: "",
+			auteurLabel: "M. MOLAC",
+			auteurGroupe: "LT",
+			alineaLabel: "",
+			missionLabel: "",
+			discussionCommune: "",
+			discussionCommuneAmdtPositon: "",
+			discussionCommuneSsAmdtPositon: "",
+			discussionIdentique: "",
+			discussionIdentiqueAmdtPositon: "",
+			discussionIdentiqueSsAmdtPositon: "",
+			position: "1341/1441"
+		}, {
+			place: "Après l'article 54",
+			sort: "",
+			numero: "316",
+			parentNumero: "",
+			auteurLabel: "Mme OBONO",
+			auteurGroupe: "FI",
+			alineaLabel: "",
+			missionLabel: "",
+			discussionCommune: "",
+			discussionCommuneAmdtPositon: "",
+			discussionCommuneSsAmdtPositon: "",
+			discussionIdentique: "",
+			discussionIdentiqueAmdtPositon: "",
+			discussionIdentiqueSsAmdtPositon: "",
+			position: "1342/1441"
+		}, {
+			place: "Article 55",
+			sort: "",
+			numero: "259",
+			parentNumero: "",
+			auteurLabel: "M. COLLARD",
+			auteurGroupe: "NI",
+			alineaLabel: "S",
+			missionLabel: "",
+			discussionCommune: "",
+			discussionCommuneAmdtPositon: "",
+			discussionCommuneSsAmdtPositon: "",
+			discussionIdentique: "14095",
+			discussionIdentiqueAmdtPositon: "debut",
+			discussionIdentiqueSsAmdtPositon: "",
+			position: "1343/1441"
+		}, {
+			place: "Article 55",
+			sort: "",
+			numero: "396",
+			parentNumero: "",
+			auteurLabel: "M. NURY",
+			auteurGroupe: "LR",
+			alineaLabel: "S",
+			missionLabel: "",
+			discussionCommune: "",
+			discussionCommuneAmdtPositon: "",
+			discussionCommuneSsAmdtPositon: "",
+			discussionIdentique: "14095",
+			discussionIdentiqueAmdtPositon: "milieu",
+			discussionIdentiqueSsAmdtPositon: "",
+			position: "1344/1441"
+		}],
+		amendementsByNum: null
+	}
+}
---
+++ src/fixtures/ProchainADiscuter.fixture.ts
@@ -0,0 +1,18 @@
+/**
+ * @author Nelson Herbin <nelson@herbin.info>
+ * @copyright 2018. MIT Licence
+ * 
+ */
+
+import { ProchainADiscuterInterface } from '../../dist/types/ProchainADiscuter.interface';
+
+export const ProchainADiscuterFixture: ProchainADiscuterInterface = { 
+    prochainADiscuter: { 
+            bibard: "1396", 
+            bibardSuffixe: "", 
+            numAmdt: "140", 
+            legislature: "15", 
+            organeAbrv: "AN", 
+            nbrAmdtRestant: "1" 
+        } 
+};
